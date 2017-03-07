26e93fe552f9fbc066a370f5b13ca243768b53a7
Paul Grenier <pgrenier@gmail.com>
2017-03-06 19:12:55 -0500

    Update uuid.js

--- uuid.js
+++ uuid.js
@@ -1,1 +1,2 @@
 function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b}
+function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)}
