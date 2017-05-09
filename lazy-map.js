140d7aea60d5f6915f2b47637652564cee58ed67
Paul Grenier <pgrenier@gmail.com>
2017-05-09 11:05:43 -0400

    Update lazy-map.js

--- lazy-map.js
+++ lazy-map.js
@@ -1,2 +1,9 @@
 const isIterable = (obj = {}) => typeof obj[Symbol.iterator] === 'function'
 const makeIterator = (arr = []) => arr[Symbol.iterator]()
+module.exports = fn =>
+  function * (iterable) {
+    for (const x of iterable) {
+      yield fn(x)
+    }
+  }
+  
