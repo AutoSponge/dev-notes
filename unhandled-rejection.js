271c0595d747533cbfaefa2f41b6f65efa8adf3c
Paul Grenier <pgrenier@gmail.com>
2017-07-19 13:20:41 -0400

    Update unhandled-rejection.js

--- unhandled-rejection.js
+++ unhandled-rejection.js
@@ -1,12 +1,26 @@
-const callsite = require('callsite-record')
+// const callsite = require('callsite-record')
 
 // useful for making sure we handle our errors
-process.on('unhandledRejection', err => {
-  console.log(
-    callsite({ forError: err }).renderSync({
-      stackfilter (frame) {
-        return !frame.getFileName().includes('node_modules')
-      }
-    })
-  )
-})
+// process.on('unhandledRejection', err => {
+//   console.log(
+//     callsite({ forError: err }).renderSync({
+//       stackfilter (frame) {
+//         return !frame.getFileName().includes('node_modules')
+//       }
+//     })
+//   )
+// })
+
+// generic closure
+(function (global) {  
+  if (global.process) {
+    process.on('unhandledRejection', handleError)
+  } else {
+    if (global.window) {
+      global.window.addEventListener('unhandledrejection', handleError)
+    }
+  }
+  function handleError(event) {
+    console.log(event)
+  }
+}(this))
