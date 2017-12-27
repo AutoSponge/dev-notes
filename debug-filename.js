890677f245a3c997b5f0ce4bd5bfa7a7bf90f0dd
Paul Grenier <pgrenier@gmail.com>
2017-12-27 11:00:52 -0500

    Update debug-filename.js

--- debug-filename.js
+++ debug-filename.js
@@ -1,2 +1,3 @@
 const debug = require('debug')(__filename.replace(/^.*[/]([^.]+)\.js/g, '$1')) // file-name
 const debug = require('debug')(__filename.replace(/^.*[/](.*)[/]([^.]+)\.js/g, '$1:$2')) // folder:file-name
+const debug = require('debug')(__filename.replace(/^.*[/]([^/]+)\.js$/, '$1'))
